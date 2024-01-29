e06119c5ce572ea274251b306b36e8465eaeaea7
arielferen <arieliten85@gmail.com>
2024-01-29 12:04:29 -0300

    add limit user query

--- Consumo de APIs/src/controllers/user.controllers.js
+++ Consumo de APIs/src/controllers/user.controllers.js
@@ -2,6 +2,8 @@ const { default: axios } = require("axios");
 const { response, request } = require("express");
 
 const getUsersMostPopular = async (req = request, res = response) => {
+  const { limit } = req.query;
+
   try {
     const response = await axios.get(
       `${process.env.API_URL_GIT}/users/google/repos`
@@ -14,7 +16,10 @@ const getUsersMostPopular = async (req = request, res = response) => {
       (a, b) => b.stargazers_count - a.stargazers_count
     );
 
-    res.json(reposSorted);
+    // Get only the specified number of repositories based on the limit
+    const limitedRepos = limit ? repos.slice(0, parseInt(limit)) : repos;
+
+    res.json(limitedRepos);
   } catch (error) {
     console.error("Error al obtener los repositorios:", error.message);
   }
